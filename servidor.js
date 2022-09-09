60742e6a6ec5c935f9115c36af66bcb5efd7ff47
Juan Gracia <juangracia.eco@gmail.com>
2022-09-08 21:51:50 -0300

    arreglo

--- servidor.js
+++ servidor.js
@@ -1,127 +1,39 @@
-//------------------------------------------------Contenedor, get y random element from array
-const fs = require('fs');
+//------------------------------------------------Server, get y random element from array
 
-class Contenedor {
-    constructor (name) {
-        this.name = name;
-    }
+const express = require ('express')
 
-    async save(informacion) {
-        try {
-            let contenido = await fs.promises.readFile(`./${this.name}`, 'utf-8');
-            let contenidoJson = JSON.parse(contenido);
-            let ultimoIndice = contenidoJson.length - 1;
-            let ultimoId = contenidoJson[ultimoIndice].id;
-            informacion.id = ++ultimoId;
-            let id = informacion.id
-            contenidoJson.push(informacion);
-            await fs.promises.writeFile(`./${this.name}`, JSON.stringify(contenidoJson));
-            return id;
-
-        }
-        catch(error) {
-            console.log(error.message)
-        }
-    }
-
-    async getById(id) {
-        try {
-            let contenido = await fs.promises.readFile(`./${this.name}`, 'utf-8');
-            let contenidoJson = JSON.parse(contenido);
-            let contenidoExtraido;
-            contenidoJson.forEach( ell => {
-                if(ell.id == id) {
-                    contenidoExtraido = ell;
-                }
-            });
-            
-            return contenidoExtraido;
-        }
-        catch(error) {
-            console.log(error.message);
-        }
-    }
-
-    async getAll() {
-        try {
-            let contenido = await fs.promises.readFile(`./${this.name}`, 'utf-8');
-            let contenidoJson = JSON.parse(contenido);
-            return contenido
-        }
-        catch(error) {
-            console.log(error.message);
-        }
-    }
-
-    async deleteById(id) {
-        try {
-            let contenido = await fs.promises.readFile(`./${this.name}`, 'utf-8');
-            let contenidoJson = JSON.parse(contenido);
-            let indice = --id
-            contenidoJson.splice(indice,1);
-            contenidoJson.forEach(el => {
-                if(el.id > id) {
-                    el.id--;
-                }
-            })
-            await fs.promises.writeFile(`./${this.name}`, JSON.stringify(contenidoJson));
-        }
-        catch(error) {
-            console.log(error.message);
-        }
-    }
-
-    async deleteAll() {
-        try {
-            let contenido = await fs.promises.readFile(`./${this.name}`, 'utf-8');
-            let contenidoJson = JSON.parse(contenido);
-            contenidoJson.splice(0,contenidoJson.length);
-            console.log(contenidoJson);
-            await fs.promises.writeFile(`./${this.name}`, JSON.stringify(contenidoJson));
-        }
-        catch(error) {
-            console.log(error.message);
-        }
-    }
-
-}
-
-let contenedor = new Contenedor('productos.json');
-
-let infoNueva = {
-    id : 1, 
-    title: "goma",
-    price: 10
-}
-
-contenedor.getById(2).then(res => {
-    console.log(res);
-})
-
-contenedor.getAll().then(res => {
-    console.log(res);
-})
-
-contenedor.deleteById(1);
-
-contenedor.deleteAll();
-
-contenedor.save(infoNueva).then( res => {
-    console.log(res);
-});
-
-let randomizado = "Regla";
-
-//----------------------------------------------------Server
-
-const express = require('express');
+const Contenedor = require('./contenedor')
 
 const PORT = process.env.PORT || 8080;
 
-const app = express();
+const app = express()
+
+const contenedor = new Contenedor('./productos.txt')
+
+const server = app.listen(PORT, () => {
+    console.log('Servidor corriendo en el puerto: ' + PORT)
+})
 
 app.get('/', (req,res) => {
-    res.send(`Esta es la pagina principal y el producto random es (${randomizado})`)
+    res.send('Esta es la pagina principal')
+})
+
+app.get('/productos', async (req,res) =>{
+    contenedor.getAll().then(resp => 
+        res.send(resp)
+    )
+})
+
+app.get('/productoRandom', async (req,res) =>{
+    let total;
+    let prodRandom
+    contenedor.getAll().then(r => {
+        total = r.length
+        prodRandom = Math.floor(Math.random() * total + 1)
+        contenedor.getById(prodRandom).then( prod =>{
+            res.send(prod)
+        })
+    })
 })
 
 app.get('/login', (req,res) => {
